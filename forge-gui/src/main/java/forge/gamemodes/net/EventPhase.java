36553e6cb141637be5191087bedcdecfd95ac0ef
nekr0s <alexanderrusev23@gmail.com>
2026-08-13 19:20:38 +0300

    feat: add TOURNAMENT_IN_PROGRESS, ROUND_IN_PROGRESS, TOURNAMENT_COMPLETE to EventPhase

--- forge-gui/src/main/java/forge/gamemodes/net/EventPhase.java
+++ forge-gui/src/main/java/forge/gamemodes/net/EventPhase.java
@@ -3,5 +3,8 @@ package forge.gamemodes.net;
 public enum EventPhase {
     LOBBY_GATHER,
     DRAFTING,
-    POOL_DISTRIBUTION
+    POOL_DISTRIBUTION,
+    TOURNAMENT_IN_PROGRESS,
+    ROUND_IN_PROGRESS,
+    TOURNAMENT_COMPLETE
 }
