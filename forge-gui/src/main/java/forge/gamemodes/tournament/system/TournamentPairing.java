c5f5e5529af74be36b3a7edfd951ac4e061b2c35
nekr0s <alexanderrusev23@gmail.com>
2026-08-17 19:58:08 +0300

    Make TournamentPairing serializable

--- forge-gui/src/main/java/forge/gamemodes/tournament/system/TournamentPairing.java
+++ forge-gui/src/main/java/forge/gamemodes/tournament/system/TournamentPairing.java
@@ -1,5 +1,6 @@
 package forge.gamemodes.tournament.system;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,8 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import forge.LobbyPlayer;
 import forge.game.GameOutcome;
 
-public class TournamentPairing {
+public class TournamentPairing implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int round;
     private boolean bye = false;
     private final List<TournamentPlayer> pairedPlayers = new ArrayList<>();
